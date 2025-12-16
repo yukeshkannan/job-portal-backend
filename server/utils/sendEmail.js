befde72a0f73a87992973c81fa8d760534314736
yukesh <yukeshyukesh785@gmail.com>
2025-12-16 16:01:51 +0530

    Fix email timeout by adding secure config

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -5,6 +5,7 @@ const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT,
+        secure: process.env.SMTP_PORT == 465, // true for 465, false for other ports
         auth: {
             user: process.env.SMTP_EMAIL,
             pass: process.env.SMTP_PASSWORD
